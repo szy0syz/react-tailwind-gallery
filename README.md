c490886d7bf2a84fe4c92b74c1df2880f7321e9d
Jerry <szy0syz@gmail.com>
2020-09-02 11:03:33 +0800

    Initial react & tailwind

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-tailwind-gallery
+
+- `npm i -D tailwindcss postcss-cli autoprefixer`
+- `npx tailwind init tailwind.js --full`
+- `touch postcss.config.js`
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="m-5 max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+    </div>
+  );
+}
+
+export default App;
